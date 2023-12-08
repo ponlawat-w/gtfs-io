9c1b4a39dc727b4d9276a7949e5040850c989617
ponlawat-w <ponlawat_w@outlook.co.th>
2023-12-08 10:56:52 +0700

    0.0.9 - Updated translation

--- src/files/translation.ts
+++ src/files/translation.ts
@@ -1,17 +1,42 @@
-/** In regions that have multiple official languages, transit agencies/operators typically have language-specific names and web pages. In order to best serve riders in those regions, it is useful for the dataset to include these language-dependent values.2 */
+import { GTFSTableName } from '../file-info';
+
+type TranslationByRecordID = {
+  /** Defines the record that corresponds to the field to be translated. */
+  record_id: string,
+  /** Helps the record that contains the field to be translated when the table doesn’t have a unique ID. */
+  record_sub_id: string|number,
+  /** Instead of defining which record should be translated by using `record_id` and `record_sub_id`, this field can be used to define the value which should be translated. */
+  field_value?: undefined|''
+};
+
+type TranslationByFieldValue = {
+  /** Defines the record that corresponds to the field to be translated. */
+  record_id?: undefined|'',
+  /** Helps the record that contains the field to be translated when the table doesn’t have a unique ID. */
+  record_sub_id?: undefined|'',
+  /** Instead of defining which record should be translated by using `record_id` and `record_sub_id`, this field can be used to define the value which should be translated. */
+  field_value?: string
+};
+
+type TranslationOfFeedInfo = {
+  /** Defines the table that contains the field to be translated. */
+  table_name: 'feed_info',
+  /** Defines the record that corresponds to the field to be translated. */
+  record_id?: undefined,
+  /** Helps the record that contains the field to be translated when the table doesn’t have a unique ID. */
+  record_sub_id?: undefined,
+  /** Instead of defining which record should be translated by using `record_id` and `record_sub_id`, this field can be used to define the value which should be translated. */
+  field_value?: undefined
+};
+
+/** In regions that have multiple official languages, transit agencies/operators typically have language-specific names and web pages. In order to best serve riders in those regions, it is useful for the dataset to include these language-dependent values. */
 export type GTFSTranslation = {
   /** Defines the table that contains the field to be translated. */
-  table_name: string,
+  table_name: GTFSTableName,
   /** Name of the field to be translated. */
   field_name: string,
   /** Language of translation. */
   language: string,
   /** Translated value. */
-  translation: string,
-  /** Defines the record that corresponds to the field to be translated. */
-  record_id?: string,
-  /** Helps the record that contains the field to be translated when the table doesn’t have a unique ID. */
-  record_sub_id?: string,
-  /** Instead of defining which record should be translated by using `record_id` and `record_sub_id`, this field can be used to define the value which should be translated. */
-  field_value?: string
-};
+  translation: string
+} & (TranslationByRecordID | TranslationByFieldValue | TranslationOfFeedInfo);
